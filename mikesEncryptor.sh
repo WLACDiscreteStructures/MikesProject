1e5f061cd1d66c591bae4c368b9cb24214c15c22
Mike The Inventor <mvivirito@gmail.com>
2019-10-22 22:48:02 -0700

    initial commit

---
+++ mikesEncryptor.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+dialog --clear --inputbox "Please enter a string you would like to convert : " 10 60 2> theString.tmp  
+
+clear       
+
+theString=$( cat theString.tmp)
+
+dialog --clear --inputbox "Choose an option :\n
+1) md5 sum\n
+2) sha256\n
+3) sha512" 10 60 2> choice.tmp  
+
+clear       
+
+encryptionChoice=$( cat choice.tmp)
+echo $encryptionChoice
+echo $theString
+
+exit 0
